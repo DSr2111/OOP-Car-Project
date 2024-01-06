e7e59bc6ce9d5e2cd081706776a677068f33fde8
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-01-06 15:53:22 -0500

    Capital letters

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //create a class for vehicle, with props of make, model, year
 //methods honk and toString needed
 
-class vehicle {
+class Vehicle {
   constructor(make, model, year) {
     this.make = make;
     this.model = model;
@@ -19,7 +19,7 @@ class vehicle {
 //Create a class for a car. The Car class should inherit from Vehicle and each
 //car instance should have a property called numWheels which has a value of 4.
 
-class car extends vehicle {
+class Car extends Vehicle {
   constructor(make, model, year) {
     super(make, model, year);
     this.numWheels = 4;
