1a5c64d65fd02d819f3aaeae358f9200dc56a038
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-01-06 15:14:37 -0500

    Comment for guide and initial code

---
+++ script.js
@@ -0,0 +1,10 @@
+//create a class for vehicle, with props of make, model, year
+//methods honk and toString needed
+
+class vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+}
